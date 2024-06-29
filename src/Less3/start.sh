e7f0a1a95e5c63c38559ced3f46685afed3671b8
CGsama <cgsama@outlook.com>
2024-06-29 18:37:39 -0400

    Update Dockerfile, Less3.csproj, and start.sh

--- src/Less3/start.sh
+++ src/Less3/start.sh
@@ -3,4 +3,5 @@ mkdir -p /data/temp && echo ok
 mkdir -p /data/logs && echo ok
 cp -n /app/less3.db /data/less3.db && echo ok
 systemctl start nginx && echo ok
+export ENV LANG=en_US.UTF-8 && ok
 dotnet Less3.dll && echo ok
